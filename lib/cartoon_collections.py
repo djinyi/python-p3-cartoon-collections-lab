e85931ba5cbda69b33f7a5480b5cd78242c538e9
Daisy Yi <daisyyi@rocketmail.com>
2025-02-26 18:05:23 -0700

    problem 1 solved

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,5 +1,7 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    if dwarves:
+        for dwarf in dwarves:
+            print(f"{dwarves.index(dwarf)+1}. {dwarf}")
 
 def summon_captain_planet():
     pass
